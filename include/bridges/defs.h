64d46e5787b164d08ad70d3a05b191f0d1849a59
Alexander DuPree <alexander.j.dupree@gmail.com>
2020-03-29 19:45:46 -0700

    Initial Commit

---
+++ include/bridges/defs.h
@@ -0,0 +1,18 @@
+///////////////////////////////////////////////////////////////////////////////
+//  File    :   defs.h
+//  Brief   :   Common includes and constants defined for the project
+//  Author  :   Alexander DuPree
+//  License :   MIT
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef BRIDGES_DEFS_H
+#define BRIDGES_DEFS_H
+
+#define BRIDGES_VERSION_MAJOR 0
+#define BRIDGES_VERSION_MINOR 1
+#define BRIDGES_VERSION_PATCH 0
+
+#define BRIDGES_VERSION "0.1.0"
+
+
+#endif // BRIDGES_DEFS_H
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+///////////////////////////////////////////////////////////////////////////////
+//  File    :   main.cpp
+//  Brief   :   Entry Point for Bridges HTTP server
+//  Author  :   Alexander DuPree
+//  License :   MIT
+///////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <bridges/defs.h>
+
+int main(int argc, char** argv)
+{
+    std::cout << BRIDGES_VERSION << std::endl;
+}
